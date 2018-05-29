a955d35411fc05cdfaead24d2eec889865324060
LeonIAGT <leon.alvarez@iagt.es>
2018-05-29 07:55:15 +0200

    FileSystemUtil , Base generator

---
+++ src/Generators/BaseGenerator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Amstaff\Generator\Generators;
+
+use Amstaff\Generator\Utils\FileSystemUtil;
+
+class BaseGenerator
+{   
+    public function generateFile ($path, $file, $content) {
+        FileSystemUtil::createDirectoryIfNotExist($path);
+        $this->rollbackOldFile($path, $file);
+        // TODO GENERATE FILE WITH CONTENT AND STUBS
+    }
+
+    public function rollbackOldFile($path, $file)
+    {
+        if (file_exists($path.$file)) {
+            if (!copy($path.$file, now()->format('d_m_Y_h_i_s') . '_' .$path.$file . '._bkp')) {
+                return false;
+            } else {
+                return FileSystemUtil::deleteFile($path, $file);
+            }
+        }
+        return false;
+    }
+
+
+}
---
+++ src/Utils/FileSystemUtil.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Amstaff\Generator\Utils;
+
+class FileSystemUtil
+{
+    public static function createFile($path, $file, $contents)
+    {
+        self::createDirectoryIfNotExist($path,true);
+        $path = $path.$file;
+        file_put_contents($path, $contents);
+    }
+
+    public static function createDirectoryIfNotExist($path, $replace = false)
+    {
+        if (file_exists($path) && $replace) {
+            rmdir($path);
+        }
+        if (!file_exists($path)) {
+            mkdir($path, 0755, true);
+        }
+    }
+
+    public static function deleteFile($path, $file)
+    {
+        if (file_exists($path.$file)) {
+            return unlink($path.$file);
+        }
+        return false;
+    }
+}
